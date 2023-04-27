7fdcee3396a96ca0b98a28925a7bab716d61d037
Luis Durante <luisdurante2@outlook.com>
2023-04-26 22:56:37 -0300

    :sparkles: Add postgres database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { databaseConfiguration } from './common/constants';
+import { User } from './modules/users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot(databaseConfiguration), UsersModule],
   controllers: [],
   providers: [],
 })
