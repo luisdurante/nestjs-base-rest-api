b96f66d0813657fc9d57924d1a04cda74bf21b55
Luis Durante <luisdurante2@outlook.com>
2023-04-28 12:59:21 -0300

    :sparkles: Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { databaseConfiguration } from './common/constants';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfiguration), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfiguration),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
