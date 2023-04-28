8d19ca42f5c70642b5bcc8be02ee7db659083fb2
Luis Durante <luisdurante2@outlook.com>
2023-04-28 12:43:57 -0300

    :fire: Remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { databaseConfiguration } from './common/constants';
-import { User } from './modules/users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfiguration), UsersModule],
