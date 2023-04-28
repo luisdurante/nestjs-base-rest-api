b500dfdad008314abdfabce8b841ddb1c120b201
Luis Durante <luisdurante2@outlook.com>
2023-04-28 12:41:59 -0300

    :sparkles: Add create user method
    Add a method on users.service responsible for creating new users

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  readonly email: string;
+  readonly name: string;
+  readonly password: string;
+  readonly birthDate: Date;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
