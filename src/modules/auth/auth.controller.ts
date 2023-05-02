7d1f13b62d14bcbd69b94e515dfb43b0179df23b
Luis Durante <luisdurante2@outlook.com>
2023-05-02 16:21:14 -0300

    :sparkles: Add password authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  login(@Request() request) {
+    return this.authService.login(request.user);
+  }
+
   @Post('signup')
   signup(@Body() createUserDto: CreateUserDto) {
     return this.authService.signup(createUserDto);
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
@@ -8,8 +9,27 @@ import { User } from '../users/entities/user.entity';
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
+  login(user: User) {
+    // TODO: Add JWT token
+    return user;
+  }
+
   async signup(signupDto: CreateUserDto): Promise<User> {
     signupDto.password = await bcrypt.hash(signupDto.password, 10);
     return this.usersService.create(signupDto);
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (!user) return null;
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) return null;
+
+    delete user['password'];
+
+    return user;
   }
 }
