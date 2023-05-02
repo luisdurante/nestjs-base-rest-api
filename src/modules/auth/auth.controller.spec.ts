12f1a93839c6a2345b51bd9f5f48b3c76be3d4ed
Luis Durante <luisdurante2@outlook.com>
2023-05-02 18:52:12 -0300

    :white_check_mark: Add auth controller tests

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,20 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersMock } from '../../common/test/users-mock';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
+
+  const usersMock = new UsersMock();
+
+  const authServiceMock = {
+    signup: jest.fn(),
+    login: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthController,
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return a valid user with their access token', async () => {
+      // Arrange
+      const loginMock = {
+        access_token: usersMock.validAccessToken,
+        user: usersMock.getValidUser(),
+      };
+
+      jest.spyOn(authService, 'login').mockReturnValue(loginMock);
+
+      // Act
+      const login = authController.login(loginMock.user);
+
+      // Assert
+      expect(login).toMatchObject(loginMock);
+      expect(authService.login).toBeCalledTimes(1);
+    });
+  });
+
+  describe('login', () => {
+    it('should return a created user', async () => {
+      // Arrange
+      const userMock = usersMock.getValidUser();
+      const createUserInput = {
+        email: userMock.email,
+        name: userMock.name,
+        birthDate: userMock.birthDate,
+        password: userMock.email,
+      };
+
+      authServiceMock.signup.mockReturnValue(userMock);
+
+      // Act
+      const user = authController.signup(createUserInput);
+
+      // Assert
+      expect(user).toMatchObject(userMock);
+      expect(authService.signup).toBeCalledTimes(1);
+    });
   });
 });
