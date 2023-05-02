4bd9f2438396f7a3ba1d348cb8c4289e8bf7216b
Luis Durante <luisdurante2@outlook.com>
2023-05-02 17:43:28 -0300

    :fire: Remove create users endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -21,10 +21,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
   @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
